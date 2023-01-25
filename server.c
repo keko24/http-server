34e8ac4c29a2d83b3c048b8c6810a5dc9234c091
keko24 <kotevski_andrej@hotmail.com>
2023-01-25 16:37:19 +0100

    Finished the function that reads the initial line of the HTTP request and started work on the header parser.

--- server.c
+++ server.c
@@ -6,7 +6,11 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #define BUFF_SIZE 256
+#define INIT_LINE_ARGS 3
+#define HEAD_LINES 10
+#define HEAD_LINE_ARGS 10
 
+char get_ch(int);
 int read_line(int, char *);
 void process_client_message(int);
 
@@ -74,16 +78,68 @@ int read_line(int sock, char *buffer) {
     return count;
 }
 
-void process_client_message(int sock) {
-    int bytes, level = 0;
-    char buffer[BUFF_SIZE];
-    bzero(buffer, BUFF_SIZE);
+void parse_initial(char *initialLine, char *initialLineParsed[INIT_LINE_ARGS]) {
+    char *saveptr;
+    initialLineParsed[0] = strtok_r(initialLine, " ", &saveptr);
+    initialLineParsed[1] = strtok_r(NULL, " ", &saveptr);
+    initialLineParsed[2] = strtok_r(NULL, " ", &saveptr);
+}
+
+int parse_header(char *headerLine, char *headerLineParsed[HEAD_LINE_ARGS], int count) { 
+    char *saveptr;
+    for (char *token = strtok_r(headerLine, " ", &saveptr); token != NULL; token = strtok_r(NULL, " ", &saveptr)) {
+        headerLineParsed[count++] = token;
+        printf("%s\n", headerLineParsed[count - 1]);
+    }
+    return count;
+}
+
+int parse_header_lines(int sock, char *buffer, char *headerLinesParsed[HEAD_LINES][HEAD_LINE_ARGS]) {
+    int headerCount = -1, count = 0, bytes;
     while ((bytes = read_line(sock, buffer)) > 0) {
         if (!strcmp(buffer, "\r\n")) {
             break;
         }
-        printf("%d %s", bytes, buffer);
+        if (buffer[0] == 'h' || buffer[0] == 'H') {
+            count = 0;
+            count = parse_header(buffer, headerLinesParsed[++headerCount], count);
+            headerLinesParsed[headerCount][count] = NULL;
+        }
+        else {
+            count = parse_header(buffer, headerLinesParsed[headerCount], count);
+            headerLinesParsed[headerCount][count] = NULL;
+        }
     }
+    return headerCount;
+}
+
+void process_client_message(int sock) {
+    int bytes, level = 0;
+    char buffer[BUFF_SIZE];
+    bzero(buffer, BUFF_SIZE);
+    if ((bytes = read_line(sock, buffer)) < 0) {
+        error("ERROR reading from socket");
+    }
+    char *initialLineParsed[INIT_LINE_ARGS];
+    parse_initial(buffer, initialLineParsed);
+    for (int i = 0; i < INIT_LINE_ARGS; i++) {
+        printf("%s\n", initialLineParsed[i]);
+    }
+    char *headerLinesParsed[HEAD_LINES][HEAD_LINE_ARGS];
+    int headerLinesCount = parse_header_lines(sock, buffer, headerLinesParsed);
+    printf("%d\n", headerLinesCount);
+    for (int i = 0; i < headerLinesCount; i++) {
+        for (int j = 0; headerLinesParsed[i][j] != NULL; j++) {
+            printf("%s ", headerLinesParsed[i][j]);
+        } 
+        printf("\n");
+    }
+    /*while ((bytes = read_line(sock, buffer)) > 0) {
+        if (!strcmp(buffer, "\r\n")) {
+            break;
+        }
+        printf("%d %s", bytes, buffer);
+    }*/
     bytes = write(sock, "I got your message.\n", 20);
     if (bytes < 0) {
         error("ERROR writing to socket");
